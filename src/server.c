4b0f52f230b31130008f089b135d7fc94711d014
Rémi Cohen-Scali <remi.cohen-scali@renault.com>
2024-07-02 17:19:19 +0200

    Updated server.c for adding server_id
    Fixed indentation
    Added arg for server_id
    Check validity of server_id
    Changed all other args nr
    Using server_id for raising MSb of packet nr
    Displaying server_id with packet nr

--- src/server.c
+++ src/server.c
@@ -30,17 +30,20 @@
 
 
 
-static void DieWithError(char* errorMessage)
+static void
+DieWithError(char* errorMessage)
 {
   fprintf(stderr, "%s\n", errorMessage);
   exit(EXIT_FAILURE);
 }
 
 
-int main(int argc, char *argv[])
+int
+main(int argc, char *argv[])
 {
   SOCKET sock;
   struct addrinfo *multicastAddr;
+  int 	    server_id;		    /* Id of server */
   char*     multicastIP;            /* Arg: IP Multicast address */
   char*     multicastPort;          /* Arg: Server port */
   char*     sendString;             /* Arg: String to multicast */
@@ -50,54 +53,49 @@ int main(int argc, char *argv[])
 
   int i;
   
-  if ( argc < 5 || argc > 6 )
+  if ( argc < 6 || argc > 7 )
     {
-      fprintf(stderr, "Usage: %s <Multicast Address> <Port> <packetsize> <defer_ms> [<TTL>]\n", argv[0]);
+      fprintf(stderr, "Usage: %s <server_id> <Multicast Address> <Port> <packetsize> <defer_ms> [<TTL>]\n", argv[0]);
       exit(EXIT_FAILURE);
     }
 
-
-  multicastIP   = argv[1];             /* First arg:   multicast IP address */
-  multicastPort = argv[2];             /* Second arg:  multicast port */
-  sendStringLen = atoi(argv[3]);   
-  defer_ms = atoi(argv[4]);
+  server_id     = atoi(argv[1]);
+  if (server_id < 0 || server_id > 3)
+    DieWithError("server ID have to be 0, 1, 2 or 3 !");
+  multicastIP   = argv[2];             /* First arg:   multicast IP address */
+  multicastPort = argv[3];             /* Second arg:  multicast port */
+  sendStringLen = atoi(argv[4]);   
+  defer_ms = atoi(argv[5]);
    
   /* just fill this with some byte */
   sendString = (char*)calloc(sendStringLen, sizeof(char));
-  for(i = 0; i<sendStringLen; ++i)
-    sendString[i]= 's';
+  for (i = 0; i < sendStringLen; ++i)
+    sendString[i] = 's';
      	
-    	
-
-  multicastTTL  = (argc == 6 ?         /* Fourth arg:  If supplied, use command-line */
-		   atoi(argv[5]) : 1); /* specified TTL, else use default TTL of 1 */
-
-
+  multicastTTL = (argc == 7 ?         /* Fourth arg:  If supplied, use command-line */
+		  atoi(argv[6]) : 1); /* specified TTL, else use default TTL of 1 */
 
   sock = mcast_send_socket(multicastIP, multicastPort, multicastTTL, &multicastAddr);
-  if(sock == -1 )
-      DieWithError("mcast_send_socket() failed");
+  if (sock == -1)
+    DieWithError("mcast_send_socket() failed");
   
-
-  int nr=0;
+  int nr = server_id << 30;
   for (;;) /* Run forever */
     {
-      int*  p_nr = (int*)sendString;
+      unsigned int* p_nr = (int*)sendString;
       *p_nr = htonl(nr);
  
-      if ( sendto(sock, sendString, sendStringLen, 0,
-		  multicastAddr->ai_addr, multicastAddr->ai_addrlen) != sendStringLen )
+      if (sendto(sock, sendString, sendStringLen, 0,
+		 multicastAddr->ai_addr, multicastAddr->ai_addrlen) != sendStringLen)
 	DieWithError("sendto() sent a different number of bytes than expected");
         
-      fprintf(stderr, "packet %d sent\n", nr);
+      fprintf(stderr, "packet %u/%u sent\n", (unsigned)nr >> 30, nr & 0x3FFFFFFF);
       nr++;
 #ifdef UNIX
       usleep(defer_ms*1000); 
-#else 
+#elif WIN32 
       Sleep (defer_ms);
 #endif 
-
-      
     }
 
   /* NOT REACHED */
